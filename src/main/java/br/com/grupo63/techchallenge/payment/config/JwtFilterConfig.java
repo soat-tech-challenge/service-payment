2d6e5b708596a624884640c66316451371273deb
Henrique Zaim <henrique.zaim@newgo.com.br>
2024-01-27 19:12:35 -0300

    feat: added jwt config

---
+++ src/main/java/br/com/grupo63/techchallenge/payment/config/JwtFilterConfig.java
@@ -0,0 +1,24 @@
+package br.com.grupo63.techchallenge.payment.config;
+
+import br.com.grupo63.techchallenge.common.config.JwtFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class JwtFilterConfig {
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Bean
+    public FilterRegistrationBean<JwtFilter> jwtFilterFilterRegistrationBean() {
+        FilterRegistrationBean<JwtFilter> jwtFilterFilterRegistrationBean = new FilterRegistrationBean<>();
+        jwtFilterFilterRegistrationBean.setFilter(new JwtFilter(jwtService));
+        jwtFilterFilterRegistrationBean.setUrlPatterns(Arrays.asList("/orders", "/payments/initialize"));
+        return jwtFilterFilterRegistrationBean;
+    }
+}
---
+++ src/main/java/br/com/grupo63/techchallenge/payment/config/JwtService.java
@@ -0,0 +1,8 @@
+package br.com.grupo63.techchallenge.payment.config;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService extends br.com.grupo63.techchallenge.common.config.JwtService {
+
+}
