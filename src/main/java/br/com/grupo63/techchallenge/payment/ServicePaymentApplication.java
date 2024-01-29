c2b2c4523a447ea6b0967896a41f347ee6453482
g-otn <44736064+g-otn@users.noreply.github.com>
2024-01-29 12:33:02 -0300

    fix: prepare cloud

--- src/main/java/br/com/grupo63/techchallenge/payment/ServicePaymentApplication.java
+++ src/main/java/br/com/grupo63/techchallenge/payment/ServicePaymentApplication.java
@@ -1,14 +1,25 @@
 package br.com.grupo63.techchallenge.payment;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@OpenAPIDefinition(
+		info = @Info(title = "${info.name}", description = "${info.description}", version = "${info.version}"),
+
+		servers ={
+				@Server(url = "${server.servlet.context-path:}", description = "Current URL"),
+				@Server(url = "localhost:${server.port:8080}${server.servlet.context-path:}", description = "Localhost"),
+				@Server(url = "${app.docs-api-url:(no value)}${server.servlet.context-path:}", description = "Custom URL from env")
+		})
 @SecurityScheme(
 		name = "bearerAuth",
 		type = SecuritySchemeType.HTTP,
--- src/main/java/br/com/grupo63/techchallenge/payment/gateway/status/IStatusGateway.java
+++ src/main/java/br/com/grupo63/techchallenge/payment/gateway/status/IStatusGateway.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "status", url = "${urls.baseurl-production}")
+@FeignClient(name = "status", url = "${app.api-url.production}")
 public interface IStatusGateway {
 
-    @RequestMapping(method = RequestMethod.POST, value = "${urls.status}/advance-status", consumes = "application/json")
+    @RequestMapping(method = RequestMethod.POST, value = "/status/advance-status", consumes = "application/json")
     void advanceStatus(@RequestParam("orderId") Long orderId);
 
 }
